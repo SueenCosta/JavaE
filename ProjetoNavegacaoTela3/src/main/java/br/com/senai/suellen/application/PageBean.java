e94fa543a4a0906fb3480a89286e906fbbab6e78
aluno <aluno@DSN1251230711>
2022-04-19 15:34:58 -0300

    ProjetoNavegacao3

---
+++ ProjetoNavegacaoTela3/src/main/java/br/com/senai/suellen/application/PageBean.java
@@ -0,0 +1,41 @@
+package br.com.senai.suellen.application;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+
+@SuppressWarnings("serial")
+@RequestScoped
+@Named
+public class PageBean implements Serializable{
+	
+	private String page;
+	
+	public enum Pages{
+		pages_a, page_b;
+	}
+
+	public String getPage() {
+		return page;
+	}
+
+	public void setPage(String page) {
+		this.page = page;
+	}
+	
+	public String goTo() {          //CONDIÇÃO
+	
+		if("a".equals(page)) {
+			return "page_a";
+			
+		}else if("b".equals(page)) {
+			return "page_b";
+		}else {
+			return null;
+		}
+		         										
+	}
+	
+}
